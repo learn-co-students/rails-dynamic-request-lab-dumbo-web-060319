9538f7e4cf539e76efb71134342cc287a1be0c32
Alexander Schelchere <ascellie@gmail.com>
2019-06-27 16:01:27 -0400

    database setup

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,12 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+
+  def show
+    @student = find_student(params[:id])
+  end
+
+  def find_student(student_id)
+      Student.all.find(student_id)
+  end
 end
